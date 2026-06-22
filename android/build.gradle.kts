18b4dd76bcd8b69a022fdb84db9a987ea5febdbf
Ahmed Fawzy <ahmedfawzyjr@gmail.com>
2026-06-22 17:27:07 +0300

    fix: safely check project state.executed before afterEvaluate in Gradle to ensure compileSdk is set to 36

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -20,13 +20,19 @@ subprojects {
 }
 
 subprojects {
-    plugins.withId("com.android.application") {
-        val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
-        android.compileSdkVersion(36)
+    val configureAndroid = {
+        val ext = project.extensions.findByName("android")
+        if (ext != null) {
+            val android = ext as com.android.build.gradle.BaseExtension
+            android.compileSdkVersion(36)
+        }
     }
-    plugins.withId("com.android.library") {
-        val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
-        android.compileSdkVersion(36)
+    if (project.state.executed) {
+        configureAndroid()
+    } else {
+        project.afterEvaluate {
+            configureAndroid()
+        }
     }
 }
 
