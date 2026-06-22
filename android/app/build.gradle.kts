ea3de7c9a08af11c9d9a8d39c705aa7c8afb1e8e
Ahmed Fawzy <ahmedfawzyjr@gmail.com>
2026-06-22 17:04:03 +0300

    fix: override compileSdk and compileSdkVersion to 36 for app and subprojects to resolve AGP 9 compileSdk incompatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.halal_player"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
